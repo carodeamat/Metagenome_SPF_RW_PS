359c63620133f609400fa2e9eae666e13b9e2c04
carodeamat <caro.deamat@mail.utoronto.ca>
2025-01-06 22:31:07 -0500

    created script to obtain URL paths

---
+++ src/getURLs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# First argument: URL
+URLpath="$1"
+
+# Second argument: user
+URLuser=$2
+
+# Third argument: password
+URLpw=$3
+
+# Get the URL paths for each file
+wget --auth-no-challenge \
+  --user=$URLuser \
+  --password=$URLpw \
+  -O urls.html \
+  --continue \
+  $URLpath
+
+
+grep -oE 'href="[^"]+\.fq\.gz"' urls.html | \
+  sed -E 's/href="([^"]+)"/\1/' | \
+  awk -v base_url=$URLpath '{print base_url $0}' > fqurls.txt
+
+grep -oE 'href="[^"]+\.fq\.gz\.md5sums"' urls.html | \
+  sed -E 's/href="([^"]+)"/\1/' | \
+  awk -v base_url=$URLpath '{print base_url $0}' > md5urls.txt
