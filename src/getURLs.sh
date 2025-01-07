5d8b27b7458ba751db63330bb96855898224ca53
carodeamat <caro.deamat@mail.utoronto.ca>
2025-01-07 16:29:25 -0500

    editted and added comments

--- src/getURLs.sh
+++ src/getURLs.sh
@@ -9,7 +9,7 @@ URLuser=$2
 # Third argument: password
 URLpw=$3
 
-# Get the URL paths for each file
+# Get a temporary index html file
 wget --auth-no-challenge \
   --user=$URLuser \
   --password=$URLpw \
@@ -17,13 +17,19 @@ wget --auth-no-challenge \
   --continue \
   $URLpath
 
-
+# First identify the .fq.gz files in index
+# Then add the base url to each file.
+# Transfer all the file urls to a txt file.
 grep -oE 'href="[^"]+\.fq\.gz"' urls.html | \
   sed -E 's/href="([^"]+)"/\1/' | \
   awk -v base_url=$URLpath '{print base_url $0}' > fqurls.txt
 
+# First identify the .fq.gz.md5sums files in index
+# Then add the base url to each file.
+# Transfer all the file urls to a txt file.
 grep -oE 'href="[^"]+\.fq\.gz\.md5sums"' urls.html | \
   sed -E 's/href="([^"]+)"/\1/' | \
   awk -v base_url=$URLpath '{print base_url $0}' > md5urls.txt
 
+# Remove the html index file
 rm urls.html
