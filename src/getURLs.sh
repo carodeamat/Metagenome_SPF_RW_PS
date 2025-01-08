80b6ac8746a4ec305445c80e17bf5f711d261523
carodeamat <caro.deamat@mail.utoronto.ca>
2025-01-08 09:18:13 -0500

    make directories for output txt

--- src/getURLs.sh
+++ src/getURLs.sh
@@ -17,19 +17,22 @@ wget --auth-no-challenge \
   --continue \
   $URLpath
 
+mkdir fqfiles
+mkdir md5files
+
 # First identify the .fq.gz files in index
 # Then add the base url to each file.
 # Transfer all the file urls to a txt file.
 grep -oE 'href="[^"]+\.fq\.gz"' urls.html | \
   sed -E 's/href="([^"]+)"/\1/' | \
-  awk -v base_url=$URLpath '{print base_url $0}' > fqurls.txt
+  awk -v base_url=$URLpath '{print base_url $0}' > fqfiles/fqurls.txt
 
 # First identify the .fq.gz.md5sums files in index
 # Then add the base url to each file.
 # Transfer all the file urls to a txt file.
 grep -oE 'href="[^"]+\.fq\.gz\.md5sums"' urls.html | \
   sed -E 's/href="([^"]+)"/\1/' | \
-  awk -v base_url=$URLpath '{print base_url $0}' > md5urls.txt
+  awk -v base_url=$URLpath '{print base_url $0}' > md5files/md5urls.txt
 
 # Remove the html index file
 rm urls.html
