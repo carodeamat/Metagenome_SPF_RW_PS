25550196635e4de239b8c820f409cdde6d32e1c4
carodeamat <caro.deamat@mail.utoronto.ca>
2025-03-03 11:12:33 -0500

    redirected output to data directory

--- slurm/Gen_md5sums_job.sh
+++ slurm/Gen_md5sums_job.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 #SBATCH --account=def-mallev
-#SBATCH --job-name=run_md5sum      # Job name
+#SBATCH --job-name=Gen_md5sum_job      # Job name
 #SBATCH --output=%x_%j.out        # Output file for SLURM output
 #SBATCH --time=01:00:00                 # Time limit
-#SBATCH --ntasks-per-node=16               # Number of tasks per node
+#SBATCH --cpus-per-task=16               # Number of cpus per task
 #SBATCH --mem=12G                        # Memory per node
 #SBATCH --nodes=1                       # Number of nodes
 
 # Define argument
-FQ_FILES=$1
+FQ_FILES="$1"
 
-# Create results log directory if they do not exist.
-if [ ! -d "results" ]; then
-  mkdir results/
-fi
-cd results
-
+# Create data log directory if they do not exist.
+cd data
 if [ ! -d "log" ]; then
   mkdir log/
 fi
@@ -24,20 +20,22 @@ cd ../
 # Run the md5sum calculation in parallel on all fastq files
 
 # If a directory is provided as argument
-if [ -d "$FQ_FILES" ]; then
+if [ -d $FQ_FILES ]; then
   find $FQ_FILES -name "*.fq.gz" | \
-  parallel -j $SLURM_TASKS_PER_NODE \
-  --joblog results/log/gen_md5sums_log.log \
+  parallel -j $SLURM_CPUS_PER_TASK \
+  --joblog data/log/gen_md5sums_log.log \
   bash src/gen_md5sums.sh {}
 
 # Check if the argument is a text file (.txt extension)
-elif [ -f "$FQ_FILES" ] && [[ "$FQ_FILES" == *.txt ]]; then
+elif [ -f $FQ_FILES ] && [[ $FQ_FILES == *.txt ]]; then
   cat $FQ_FILES | \
-  parallel -j $SLURM_TASKS_PER_NODE \
-  --joblog results/log/gen_md5sums_log.log \
+  parallel -j $SLURM_CPUS_PER_TASK \
+  --joblog data/log/gen_md5sums_log.log \
   bash src/gen_md5sums.sh {}
 
 # If it's neither
 else
   echo "'$FQ_FILES' should be a directory or a text file with a list of existing fq files."
 fi
+
+sacct -j $SLURM_JOB_ID --format=JobID%16,Submit,Start,Elapsed,NCPUS,CPUTime,ReqMem,ExitCode,NodeList%8
