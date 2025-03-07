a058bfcbc695c40fa26ea43d1c17ffcb032d76cc
carodeamat <caro.deamat@mail.utoronto.ca>
2025-03-06 21:01:51 -0500

    edited slurm options and make out dir

--- test/code/sample_job.sh
+++ test/code/sample_job.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 #SBATCH --account=def-mallev
-#SBATCH --job-name=run_analyze
+#SBATCH --job-name=fqsample
 #SBATCH --output=test/%x_%j.out
 #SBATCH --time=01:00:00
-#SBATCH --cpus-per-task=32
-#SBATCH --mem=12G
+#SBATCH --ntasks=32
 #SBATCH --nodes=1
 
 # Load modules
 module load CCEnv
 module load StdEnv/2020
 module load seqkit
-cd $SCRATCH
 
 # Define variables
 IN_DIR=$1
 OUT_DIR=$2
 PROP=$3
 
+# create output directory if it does not exist.
+if [ ! -d $OUT_DIR ]; then
+  mkdir $OUT_DIR/
+fi
 #Sample a proportion of the reads.
 mkdir $SFQ_DIR
-ls $IN_DIR/*.fq.gz | parallel -j $SLURM_CPUS_PER_TASK \
+ls $IN_DIR/*_L01_*.fq.gz | parallel -j $SLURM_NTASKS \
     "seqkit sample -p $PROP -s 123 -o $OUT_DIR/{/} {}"
