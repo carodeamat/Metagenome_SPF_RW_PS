02d8446b863074f0e0117a9fe4e551d9ccd060e6
carodeamat <caro.deamat@mail.utoronto.ca>
2025-03-07 22:08:21 -0500

    fixed script directory and edited slurm params

--- slurm/Checksums_job.sh
+++ slurm/Checksums_job.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 #SBATCH --account=def-mallev
-#SBATCH --job-name=Checksums_job      # Job name
-#SBATCH --output=%x_%j.out        # Output file for SLURM output
-#SBATCH --time=01:00:00                 # Time limit
-#SBATCH --cpus-per-task=16               # Number of cpus per task
-#SBATCH --mem=4G                        # Memory per node
-#SBATCH --nodes=1                       # Number of nodes
+#SBATCH --job-name=Checksums_job
+#SBATCH --output=data/%x_%j.out
+#SBATCH --time=01:00:00
+#SBATCH --cpus-per-task=32
+#SBATCH --nodes=1
 
 module load CCEnv
 module load StdEnv/2023
@@ -16,4 +15,4 @@ module load r-bundle-bioconductor/3.18
 MD5_CLUSTER="data/md5sums_cluster"
 MD5_CORE="data/md5sums_core"
 
-Rscript checksums.R $MD5_CLUSTER $MD5_CORE
+Rscript src/checksums.R $MD5_CLUSTER $MD5_CORE
