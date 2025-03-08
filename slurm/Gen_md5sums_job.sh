9fd26f32d8d7a2abb78e97e9ea4433cb35f10cce
carodeamat <caro.deamat@mail.utoronto.ca>
2025-03-07 22:11:05 -0500

    added modules and edited parallel options

--- slurm/Gen_md5sums_job.sh
+++ slurm/Gen_md5sums_job.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 #SBATCH --account=def-mallev
-#SBATCH --job-name=Gen_md5sum_job      # Job name
-#SBATCH --output=%x_%j.out        # Output file for SLURM output
-#SBATCH --time=01:00:00                 # Time limit
-#SBATCH --cpus-per-task=16               # Number of cpus per task
-#SBATCH --mem=12G                        # Memory per node
-#SBATCH --nodes=1                       # Number of nodes
+#SBATCH --job-name=Gen_md5sum_job
+#SBATCH --output=data/%x_%j.out
+#SBATCH --time=00:15:00
+#SBATCH --ntasks=32
+#SBATCH --nodes=1
 
 # Define argument
-FQ_FILES="$1"
+FQ_FILES=data/fqfiles
+
+module load CCEnv
+module load StdEnv/2023
 
 # Create data log directory if they do not exist.
 cd data
@@ -22,14 +24,14 @@ cd ../
 # If a directory is provided as argument
 if [ -d $FQ_FILES ]; then
   find $FQ_FILES -name "*.fq.gz" | \
-  parallel -j $SLURM_CPUS_PER_TASK \
+  parallel -j $SLURM_NTASKS \
   --joblog data/log/gen_md5sums_log.log \
   bash src/gen_md5sums.sh {}
 
 # Check if the argument is a text file (.txt extension)
 elif [ -f $FQ_FILES ] && [[ $FQ_FILES == *.txt ]]; then
   cat $FQ_FILES | \
-  parallel -j $SLURM_CPUS_PER_TASK \
+  parallel -j $SLURM_NTASKS \
   --joblog data/log/gen_md5sums_log.log \
   bash src/gen_md5sums.sh {}
 
@@ -37,5 +39,3 @@ elif [ -f $FQ_FILES ] && [[ $FQ_FILES == *.txt ]]; then
 else
   echo "'$FQ_FILES' should be a directory or a text file with a list of existing fq files."
 fi
-
-sacct -j $SLURM_JOB_ID --format=JobID%16,Submit,Start,Elapsed,NCPUS,CPUTime,ReqMem,ExitCode,NodeList%8
