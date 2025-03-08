6118302e57a5cc423bbcd662efc700d64ed6d058
carodeamat <caro.deamat@mail.utoronto.ca>
2025-03-08 11:23:10 -0500

    removed get_sample step and quoted LANE

--- slurm/MapRef_job.sh
+++ slurm/MapRef_job.sh
@@ -23,16 +23,9 @@ cd ..
 
 export BOWTIE2_INDEXES=$REF_GENOME
 
-if [ ! -f $OUT_DIR/sample_ids_$LANE.txt ]; then
-  bash src/get_sampleID.sh $IN_DIR $OUT_DIR $LANE
-fi
-#remove the last two rows that correlate to the positive/negative control samples
-sed -i '$d' $OUT_DIR/sample_ids_$LANE.txt
-sed -i '$d' $OUT_DIR/sample_ids_$LANE.txt
-
-cat $OUT_DIR/SampleIDs/sample_ids_$LANE.txt | \
+cat $OUT_DIR/SampleIDs/sample_ids_"$LANE".txt | \
 parallel -j $SLURM_NTASKS \
 bowtie2 -x genome \
-    -1 $IN_DIR/*_$LANE_{}_1.fq.gz \
-    -2 $IN_DIR/*_$LANE_{}_2.fq.gz \
-    -S $OUT_DIR/MapRef/MapRef_$LANE_{}.sam
+    -1 $IN_DIR/*_"$LANE"_{}_1.fq.gz \
+    -2 $IN_DIR/*_"$LANE"_{}_2.fq.gz \
+    -S $OUT_DIR/MapRef/MapRef_"$LANE"_{}.sam
