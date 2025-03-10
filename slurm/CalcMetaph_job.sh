8a3d53c3ff527bcc4790971a1b647e7334af9c39
carodeamat <caro.deamat@mail.utoronto.ca>
2025-03-09 23:42:20 -0400

    added merge for vsc

--- slurm/CalcMetaph_job.sh
+++ slurm/CalcMetaph_job.sh
@@ -3,10 +3,10 @@
 #SBATCH --job-name=CalcMetaph_job
 #SBATCH --output=data/%x_%j.out
 #SBATCH --time=2:00:00
-#SBATCH --ntasks=4
+#SBATCH --ntasks=1
 #SBATCH --nodes=1
 
-IN_DIR=analysis/MetaphlanOut
+IN_DIR=analysis
 
 module load CCEnv
 module load StdEnv/2023
@@ -16,9 +16,10 @@ module load python/3.11.5
 
 source $SCRATCH/humann_venv/bin/activate
 
-merge_metaphlan_tables.py $IN_DIR/Sample_*_profile.txt > $IN_DIR/merged_abundance_table.txt
+merge_metaphlan_tables.py $IN_DIR/MetaphlanOut/Sample_*_profile.txt > $IN_DIR/MetaphlanOut/merged_abundance_table.txt
+merge_vsc_tables.py $IN_DIR/VSCout/Sample_*_vsc.txt > $IN_DIR/VSCout/merged_abundance_table.txt
 
-Rscript calculate_diversity.R --file="$IN_DIR/merged_abundance_table.txt" \
---out_directory="$IN_DIR" \
+Rscript calculate_diversity.R --file="$IN_DIR/MetaphlanOut/merged_abundance_table.txt" \
+--out_directory="$IN_DIR/MetaphlanOut" \
 --diversity="beta" \
 --metric="bray-curtis"
